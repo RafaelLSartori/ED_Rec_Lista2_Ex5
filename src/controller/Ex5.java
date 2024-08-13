d954aef62b6829308cff6adf024d4d4246efafe7
Rafael Sartori <160548884+RafaelLSartori@users.noreply.github.com>
2024-08-12 23:01:02 -0300

    Atualização

--- src/controller/Ex5.java
+++ src/controller/Ex5.java
@@ -1,15 +1,19 @@
 package controller;
 
 public class Ex5 {
-	public Ex5() {
-		super();
-	}
-	
-	public int mdc(int n1, int n2){
-		if(n1 == 0) {
-			return n1;
-		} else {
-			return mdc(n2, n1 % n2);
-		}
-	}
+    public Ex5() {
+        super();
+    }
+
+    public int mdc(int n1, int n2){
+        if(n1 == n2) {
+            return n1;
+        } else {
+            if(n1 > n2) {
+                return mdc(n1 - n2, n2);
+            } else{
+                return mdc(n1, n2 - n1);
+            }
+        }
+    }
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -2,15 +2,12 @@ package view;
 import controller.Ex5;
 
 public class Main {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-
-		Ex5 m = new Ex5();
-		int num1 = 24;
-		int num2 = 20;
-		int resp = m.mdc(num1, num2);
-		System.out.println(resp);
-		
-	}
-
+        Ex5 m = new Ex5();
+        int num1 = 24;
+        int num2 = 20;
+        int resp = m.mdc(num1, num2);
+        System.out.print(resp);
+    }
 }
